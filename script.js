6a2770b89f4e74e30511863184f8684fc40d2a4b
Luckmuc <valentin@dombret.de>
2026-06-24 19:37:00 +0200

    start adding markdown render function

--- script.js
+++ script.js
@@ -2,18 +2,19 @@ const editor = document.getElementById("editor");
 const preview = document.getElementById("preview");
 
 editor.value = "";
+preview.textContent = "";
 
 function previewUpdate() {
     const text = editor.value;
+    const html = renderMarkdown(text)
+    preview.innerHTML = html;
+}
 
-    preview.textContent = text;
+function renderMarkdown() {
+    return text;
 }
 
 editor.addEventListener("input", previewUpdate);
 
 previewUpdate();
-
-
-
-
 
