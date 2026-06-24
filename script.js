e60604dd6ab47a0e224d4e9035f3900badbff3bc
Luckmuc <valentin@dombret.de>
2026-06-24 20:18:14 +0200

    added a function called inlineDesigns to render stuff like * and ** in the preview. Coloured the editor & preview +headlines. Changed the background colour of the editor and preview. Changed the border colour of the editor and preview.

--- script.js
+++ script.js
@@ -17,17 +17,17 @@ function renderMarkdown(text) {
     const htmllines = lines.map(line => {
 
         if (line.startsWith("# ")) {
-            const content = line.slice(2);
+            const content = inlineDesigns(line.slice(2));
             return `<h1>${content}</h1>`
         }
 
         if (line.startsWith("## ")) {
-            const content = line.slice(3);
+            const content = inlineDesigns(line.slice(3));
             return `<h2>${content}</h2>`
         }
 
         if (line.startsWith("### ")) {
-            const content = line.slice(4);
+            const content = inlineDesigns(line.slice(4));
             return `<h3>${content}</h3>`
         }
 
@@ -35,10 +35,16 @@ function renderMarkdown(text) {
             return "";
         }
 
-        return `<p>${line}</p>`;
+        return `<p>${inlineDesigns(line)}</p>`;
     });
 
     return htmllines.join("\n");
+}
+
+function inlineDesigns(text) {
+    text = text.replace(/\*\*(.+?)\*\*/g, "<strong>$1</strong>");
+    text = text.replace(/\*(.+?)\*/g, "<em>$1</em>");    // tf does this do, inline suggestions doing whatever but it lowk seems to work
+    return text;
 }
 
 editor.addEventListener("input", previewUpdate);
